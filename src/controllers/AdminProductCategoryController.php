194cc7ea4baa9b7b3af59082b9464b184d1b6f37
Jacob Martin <jacobm@angelvisiontech.com>
2014-06-18 11:05:15 -0700

    Moving category tree logic to model

--- src/controllers/AdminProductCategoryController.php
+++ src/controllers/AdminProductCategoryController.php
@@ -12,37 +12,11 @@ class AdminProductCategoryController extends AdminCrudController {
 	{
 		$temp_categories = ProductCategory::orderBy('parent_id')->orderBy('order')->get();
 
-		$categories = $this->categories_tree($temp_categories);
+		$categories = ProductCategory::tree($temp_categories);
 
 		$this->data['categories'] = $categories;
 
 		return View::make($this->view('index'), $this->data);
-	}
-
-	/**
-	 * Creates a nested array of categories and their children.
-	 *
-	 * @param $categories - An Illuniate Collection of all product categories.
-	 * @param null $parent_id - Used for recursively creating branches.
-	 * @return array - The completed branch.
-	 */
-	private function categories_tree($categories, $parent_id = null)
-	{
-		$branch = array();
-
-		foreach ($categories as $category) {
-			if ($category->parent_id != $parent_id) continue;
-
-			$children = $this->categories_tree($categories, $category->id);
-
-			if (count($children)) {
-				$category->children = $children;
-			}
-
-			$branch[] = $category;
-		}
-
-		return $branch;
 	}
 
 	/**
--- src/models/ProductCategory.php
+++ src/models/ProductCategory.php
@@ -18,6 +18,14 @@ class ProductCategory extends Eloquent {
 		return $this->hasMany('ProductCategory', 'parent_id');
 	}
 
+	/**
+	 * Create the HTML for breadcrumbs for a category.
+	 *
+	 * @param Collection $categories - A collection of all categories
+	 * @param int $category_id - The current visible category
+	 * @param string $url - The URL to use for the crumbs, with {id} signifying where the category ID should go for each crumb
+	 * @return HTML of the breadcrumbs
+	 */
 	public static function crumbs($categories, $category_id, $url = null)
 	{
 		$url = (!$url) ? admin_url('products/categories/expand/{id}') : $url;
@@ -32,6 +40,32 @@ class ProductCategory extends Eloquent {
 		$crumbs = array_combine(array_reverse(array_keys($crumbs)), array_reverse($crumbs));
 
 		return View::make('products::products.categories.crumbs', compact('crumbs', 'url'));
+	}
+
+	/**
+	 * Creates a nested array of categories and their children.
+	 *
+	 * @param $categories - An Illuniate Collection of all product categories.
+	 * @param null $parent_id - Used for recursively creating branches.
+	 * @return array - The completed branch.
+	 */
+	public static function tree($categories, $parent_id = null)
+	{
+		$branch = array();
+
+		foreach ($categories as $category) {
+			if ($category->parent_id != $parent_id) continue;
+
+			$children = ProductCategory::tree($categories, $category->id);
+
+			if (count($children)) {
+				$category->children = $children;
+			}
+
+			$branch[] = $category;
+		}
+
+		return $branch;
 	}
 
 }
