f93e61accb03d0477da4d4ff58310d090bde00b4
Jacob Martin <jacobm@angelvisiontech.com>
2014-08-01 15:23:48 -0700

    Email details

--- src/views/orders/emails/details.blade.php
+++ src/views/orders/emails/details.blade.php
@@ -34,42 +34,32 @@ $Cart->load($cart);
 <hr />
 @foreach ($cart as $key=>$item)
 	<?php $product = json_decode($item['product']); ?>
-	<div class="row">
-		<div class="col-sm-3">
-			<a href="{{ url('products/' . $product->slug) }}">
-				<img src="{{ $product->images[0]->image }}" style="width:100%" />
-			</a>
-		</div>
-		<div class="col-sm-3">
-			<h4>
-				<a href="{{ url('products/' . $product->slug) }}">
-					{{ $product->name }}
-				</a>
-			</h4>
-			<hr />
-			@foreach ($Cart->getOptions($key) as $group_name=>$option)
-				<p>
-					<b>{{ $group_name }}:</b>
-				</p>
-				<p>
-					{{ $option->name }}
-				</p>
-			@endforeach
-		</div>
-		<div class="col-sm-3">
-			<h4>Price</h4>
-			<hr />
-			<h5 style="text-decoration:line-through;font-style:italic;">${{ number_format($item['fake_price'], 2) }}</h5>
-			<h3>${{ number_format($item['price'], 2) }}</h3>
-		</div>
-		<div class="col-sm-3">
-			<h4>Quantity</h4>
-			<hr />
-			{{ $item['qty'] }}
-		</div>
-	</div>
+	<p>
+		<img src="{{ $message->embed(public_path() . $product->images[0]->image) }}" style="width:240px" width="240" />
+	</p>
+	<p>
+		<a href="{{ url('products/' . $product->slug) }}">
+			{{ $product->name }}
+		</a>
+		<?php
+		$options = $Cart->getOptions($key);
+		if (count($options)) {
+			echo '(';
+			$i = 0;
+			foreach ($options as $group_name=>$option) {
+				echo $group_name . ': ' . $option->name;
+				if (++$i < count($options)) echo ', ';
+			}
+			echo ')';
+		}
+		?>
+		-
+		${{ number_format($item['price'], 2) }}
+		x
+		{{ $item['qty'] }}
+	</p>
 	<hr />
 @endforeach
-<div class="text-right">
-	<h3>Total: ${{ number_format($order->total, 2) }}</h3>
-</div>
+<p class="text-right">
+	<b>Total: ${{ number_format($order->total, 2) }}</b>
+</p>
