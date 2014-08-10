3c6cb977e199c6e3cd15d76981884d8db26efbc7
Jacob Martin <mindnumb@gmail.com>
2014-08-09 23:19:36 -0700

    Crumbs bugfix

--- src/views/products/categories/crumbs.blade.php
+++ src/views/products/categories/crumbs.blade.php
@@ -1,10 +1,11 @@
 <ol class="breadcrumb">
 	<?php $i = 0; ?>
+	<?php $url_original = $url; ?>
 	@foreach ($crumbs as $id=>$category)
 		@if (++$i < count($crumbs))
 			<li>
 				<?php
-					$url = str_replace('{id}', $id, $url);
+					$url = str_replace('{id}', $id, $url_original);
 					$url = str_replace('{slug}', $category->slug, $url);
 				?>
 				<a href="{{ $url }}">
