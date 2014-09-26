c94d9d120676fad60dd28cd20ccbee4fcbd9ecf0
Jacob Martin <MindNumb@gmail.com>
2014-09-25 17:11:12 -0700

    Extend core email template on emails

--- src/views/orders/emails/receipt.blade.php
+++ src/views/orders/emails/receipt.blade.php
@@ -1,67 +1,71 @@
-<?php
-$cart             = json_decode($order->cart, true);
-$billing_address  = json_decode($order->billing_address);
-$shipping_address = json_decode($order->shipping_address);
+@extends('core::emails.template')
 
-$Cart = App::make('Cart');
-$Cart->load($cart);
-?>
-<p>
-	Thank you for your order!  Here is your receipt.
-</p>
-<p>
-	<b>Order ID:</b>
-</p>
-<p>
-	{{ $order->id }}
-</p>
-<p>
-	<b>Order Time:</b>
-</p>
-<p>
-	{{ $order->created_at }}
-</p>
-@if (count($billing_address))
+@section('content')
+	<?php
+	$cart             = json_decode($order->cart, true);
+	$billing_address  = json_decode($order->billing_address);
+	$shipping_address = json_decode($order->shipping_address);
+
+	$Cart = App::make('Cart');
+	$Cart->load($cart);
+	?>
 	<p>
-		<b>Billed To:</b>
+		Thank you for your order!  Here is your receipt.
+	</p>
 	<p>
-	@include('products::orders.address', array('address'=>$billing_address))
-@endif
-<p>
-	<b>Shipping To:</b>
-</p>
-@include('products::orders.address', array('address'=>$shipping_address))
-<hr />
-@foreach ($cart as $key=>$item)
-	<?php $product = json_decode($item['product']); ?>
-	@if (isset($product->images) && count($product->images))
+		<b>Order ID:</b>
+	</p>
+	<p>
+		{{ $order->id }}
+	</p>
+	<p>
+		<b>Order Time:</b>
+	</p>
+	<p>
+		{{ $order->created_at }}
+	</p>
+	@if (count($billing_address))
 		<p>
-			<img src="{{ $message->embed(public_path() . $product->images[0]->image) }}" style="width:240px" width="240" />
-		</p>
+			<b>Billed To:</b>
+		<p>
+		@include('products::orders.address', array('address'=>$billing_address))
 	@endif
 	<p>
-		<a href="{{ url('products/' . $product->slug) }}">
-			{{ $product->name }}
-		</a>
-		<?php
-		$options = $Cart->getOptions($key);
-		if (count($options)) {
-			echo '(';
-			$i = 0;
-			foreach ($options as $group_name=>$option) {
-				echo $group_name . ': ' . $option->name;
-				if (++$i < count($options)) echo ', ';
-			}
-			echo ')';
-		}
-		?>
-		-
-		${{ number_format($item['price'], 2) }}
-		x
-		{{ $item['qty'] }}
+		<b>Shipping To:</b>
 	</p>
+	@include('products::orders.address', array('address'=>$shipping_address))
 	<hr />
-@endforeach
-<p class="text-right">
-	<b>Total: ${{ number_format($order->total, 2) }}</b>
-</p>
+	@foreach ($cart as $key=>$item)
+		<?php $product = json_decode($item['product']); ?>
+		@if (isset($product->images) && count($product->images))
+			<p>
+				<img src="{{ $message->embed(public_path() . $product->images[0]->image) }}" style="width:240px" width="240" />
+			</p>
+		@endif
+		<p>
+			<a href="{{ url('products/' . $product->slug) }}">
+				{{ $product->name }}
+			</a>
+			<?php
+			$options = $Cart->getOptions($key);
+			if (count($options)) {
+				echo '(';
+				$i = 0;
+				foreach ($options as $group_name=>$option) {
+					echo $group_name . ': ' . $option->name;
+					if (++$i < count($options)) echo ', ';
+				}
+				echo ')';
+			}
+			?>
+			-
+			${{ number_format($item['price'], 2) }}
+			x
+			{{ $item['qty'] }}
+		</p>
+		<hr />
+	@endforeach
+	<p class="text-right">
+		<b>Total: ${{ number_format($order->total, 2) }}</b>
+	</p>
+@stop
--- src/views/orders/emails/tracking.blade.php
+++ src/views/orders/emails/tracking.blade.php
@@ -1,9 +1,13 @@
-<p>
-	Your order #{{ $order->id }} has shipped!
-</p>
-<p>
-	Your tracking number:
-</p>
-<p>
-	{{ $order->tracking }}
-</p>
+@extends('core::emails.template')
+
+@section('content')
+	<p>
+		Your order #{{ $order->id }} has shipped!
+	</p>
+	<p>
+		Your tracking number:
+	</p>
+	<p>
+		{{ $order->tracking }}
+	</p>
+@stop
